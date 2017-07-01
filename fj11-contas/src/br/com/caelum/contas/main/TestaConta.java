7b03d8cddc236e539af3bd2f7f966bc44fe22f3a
oo6938 <oo6938@caelum57-16>
2017-07-01 16:39:24 -0300

    Testes

---
+++ fj11-contas/src/br/com/caelum/contas/main/TestaConta.java
@@ -0,0 +1,14 @@
+package br.com.caelum.contas.main;
+
+public class TestaConta {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setTitular("Everton");
+		conta.deposita(300.0);
+		if (conta.saca(200.0)) {
+			System.out.println("Sacou!");
+		} else {
+			System.out.println("Não Sacou!");
+		}
+	}
+}
