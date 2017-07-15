ba97f16f3582b83e398703f2e185fa58bcfe84d4
oo6938 <oo6938@caelum57-16>
2017-07-15 12:12:08 -0300

    interfaces - exercicios 142/145

--- fj11-contas/src/br/com/caelum/contas/TestaContas.java
+++ fj11-contas/src/br/com/caelum/contas/TestaContas.java
@@ -6,6 +6,6 @@ import br.com.caelum.javafx.api.main.SistemaBancario;
 public class TestaContas {
 
 	public static void main(String[] args) {
-		SistemaBancario.mostraTela(false);
+		SistemaBancario.mostraTela(true);
 	}
 }
